2d17f6e3b358fbfb96b2939d4a0660700fab4fe0
serdarunlu96 <71671958+serdarunlu96@users.noreply.github.com>
2023-08-07 23:24:21 +0300

    Add project files.

---
+++ ANK13Layout/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using ANK13Layout.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ANK13Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult YurtIci()
+        {
+            return View();
+        }
+
+        public IActionResult YurtDisi()
+        {
+            return View();
+        }
+        public IActionResult Ege()
+        {
+            return View();
+        }
+
+        public IActionResult Akdeniz()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
